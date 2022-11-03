8914db3840f02e7b8e3f941fe91e9a3df3551a26
David Alejandro Quintanilla Chacon <113323868+DavidxCode@users.noreply.github.com>
2022-11-03 00:18:52 -0500

    Agregando parte visual practica 1
    Agregando función timing, contador recursiva

--- practica-01.py
+++ practica-01.py
@@ -1,3 +1,10 @@
+def timing():
+    for i in range(10, 0, -1):
+        sys.stdout.write(str('%') + ' ')
+        sys.stdout.flush()
+        time.sleep(1)
+    print("\n")
+
 class Libro():
 
     def __init__(self, csv):
@@ -25,6 +32,11 @@ class Libro():
     def eliminar_libro(self):
         pass
 
-
 if __name__ == '__main__':
-    pass
+    if again("Quieres iniciar la lectura de un archivo CSV: "):
+        ruta = input(r"Ingresa una ruta absoluta de la ubicacion de tu CSV sin la extension : ") + ".csv"
+        if ruta:
+            obj = Libro(ruta)
+            print("Procesando archivo ...")
+            timing()
+     
