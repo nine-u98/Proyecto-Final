a7c1fe774e86d5ee2553e155182d401978836628
nine-u98 <danielflop98@gmail.com>
2022-11-02 22:43:09 -0500

    Estructura de practica

---
+++ practica-01.py
@@ -0,0 +1,30 @@
+class Libro():
+
+    def __init__(self, csv):
+        self.csv = csv
+    def listarLibros(self):
+        pass
+
+    def ordenarLibros(self):
+        pass
+
+    def buscarLibro(self):
+        pass
+    def buscar_num_Autores(self):
+        pass
+
+    def agregarLibro(self):
+        pass
+
+    def actualizarLibro(self):
+        pass
+
+    def guardaLibro(self):
+        pass
+
+    def eliminar_libro(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
