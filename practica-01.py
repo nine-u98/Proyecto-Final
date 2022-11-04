956cfdc0d92e17e637e72f33b32d8ba7be35dc72
David Alejandro Quintanilla Chacon <113323868+DavidxCode@users.noreply.github.com>
2022-11-04 02:46:11 -0500

    funciones apariencia

--- practica-01.py
+++ practica-01.py
@@ -1,3 +1,43 @@
+import sys
+from os import system
+import pandas as pd
+import time
+
+def again(x="Quieres volver al menu? : "):
+    if input(x).lower().strip() in ['s', 'si']:
+        system('clear')
+        if x == "Quieres volver al menu? : ":
+            menu()
+        return True
+
+
+def menu():
+    print("""\
+Lista de [OPCIONES]
+Opción 1: Listar libros.
+Opción 2: Agregar libro.
+Opción 3: Eliminar libro.
+Opción 4: Buscar libro por ISBN, Titulo, Autor, Genero, Editorial.
+Opción 5: Ordenar libros por título.
+Opción 6: Buscar libros por número de autores.
+Opción 7: Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores).
+Opción 8: Guardar libros en archivo de disco duro (.txt o csv).
+Opcion 9: Finalizar programa""")
+
+
+def modlibros():
+    print("""\
+Lista de categorias
+1) Titulo
+2) Genero
+3) ISBN
+4) Editorial
+5) Autor""")
+    cat = int(input('=> '))
+
+    if 6 > cat >= 1:
+        search = input("Texto a buscar : ")
+        print(obj.buscarLibro(cat, search))
 def timing():
     for i in range(10, 0, -1):
         sys.stdout.write(str('%') + ' ')
