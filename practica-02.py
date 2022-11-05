6c382480223bc27d88567655fe2e5c376d7427cc
David Alejandro Quintanilla Chacon <113323868+DavidxCode@users.noreply.github.com>
2022-11-05 00:53:39 -0500

    Funcion conectar

--- practica-02.py
+++ practica-02.py
@@ -1,58 +1,30 @@
 import requests
 
+ 
 
 # listar pokemons involucra: nombre, habilidad y URL de la imagen
 class Pokemon:
-    def __init__(self, sel):
-        self.sel = sel
-
+    def __init__(self):
+        pass
+        
     def __conect(self, url):
         r = requests.get(url)
         status = r.status_code
         if status != 200:
             quit()
-        else:
-            return r.json()
-    
-    def __conectaAPI(self, url):
-        pass
-
-    def __str__(self) -> None:
-        pass
 
     # * Funcion - obtener los datos de generacion & forma & habilidad
-    def funcion_universal(self):
+    def func_general(self):
         pass
-
-    # ! Eliminar
-    def pokeGeneracion(self):
-        '''Listar pokemons por generación.
-        Se ingresa alguna generación (1, 2, 3, ..)
-        y se listan todos los pokemon respectivos.'''
-        pass
-
-    # ! Eliminar
-    def pokeForma(self):
-        '''Listar pokemons por forma. Se ingresa
-        alguna forma (deben sugerir valores) y se
-        listan todos los pokemons respectivos.'''
-        pass
-
-    # ! Eliminar
-    def pokeHabilidad(self):
-        '''Listar pokemons por habilidad. Se deben
-        sugerir opciones a ingresar para interactuar.'''
-
+    
     def pokeHabitat(self):
         pass
 
-    def pokeDetalles(self, nombre):
+    def pokeTipo(self, nombre):
 
         pass
 
-    def pokeTipo(self):
-        '''Listar pokemons por tipo. Se deben sugerir
-        opciones a ingresar para interactuar.'''
+    def details(self):
         pass
 
 
