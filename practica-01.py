b6df7c06379f557982ae4b0381ae25d2ad008d90
David Alejandro Quintanilla Chacon <113323868+DavidxCode@users.noreply.github.com>
2022-11-06 11:58:30 -0500

    agregando función de guardar y eliminar

--- practica-01.py
+++ practica-01.py
@@ -66,11 +66,14 @@ class Libro():
     def actualizarLibro(self):
         pass
 
-    def guardaLibro(self):
-        pass
+    def guardalibro(self):
+        self.dt.to_csv(self.libros, index=False)
+        print("Archivo guardado")
 
-    def eliminar_libro(self):
-        pass
+    def eliminar_libro(self, num): 
+        self.dt.drop(num, inplace=True)
+        self.dt.to_csv(self.libros)
+        print(f"Eliminando")
 
 if __name__ == '__main__':
     if again("Quieres iniciar la lectura de un archivo CSV: "):
